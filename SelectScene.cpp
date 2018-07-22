713e0a6534246b70816d3bbbc000241813a5468a
wanggang <wg1095541276@hotmail.com>
2018-07-22 14:56:50 +0800

    添加游戏选项

--- SelectScene.cpp
+++ SelectScene.cpp
@@ -1,6 +1,7 @@
 #include "SelectScene.h"
 #include "GameScene.h"
 #include "LoadingScene.h"
+#include "Constant.h"
 
 SelectScene* SelectScene::createScene() {
 	return SelectScene::create();
@@ -17,6 +18,13 @@ bool SelectScene::init() {
 	bg->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
 	this->addChild(bg);
 
+	//游戏选项label
+	auto gameOption = Label::createWithTTF("Game Option", "BRADHITC.TTF", 56);
+	gameOption->setColor(Color3B::BLACK);
+	gameOption->setPosition(SIZE.width / 2, SIZE.width / 2 + 300);
+	this->addChild(gameOption);
+
+
 	//模式选择菜单
 
 	//简单选项
@@ -32,7 +40,7 @@ bool SelectScene::init() {
 		auto scene = GameScene::createScene();
 		Director::getInstance()->replaceScene(scene);
 	});
-
+	//困难选项
 	auto spdifficult = Sprite::createWithSpriteFrameName("btn_finish.png");
 	auto itemdiffcult = MenuItemSprite::create(spdifficult, spdifficult, [=](Ref*) {
 		auto scene = GameScene::createScene();
@@ -40,10 +48,14 @@ bool SelectScene::init() {
 	});
 	itemdiffcult->setPositionY(-100);
 
-	
+	//皮肤选择,进入一个新的界面选择皮肤
+	auto spskin = Sprite::createWithSpriteFrameName("btn_finish.png");
+	auto itemskin = MenuItemSprite::create(spskin, spskin, [](Ref*) {});
+	itemskin->setPositionY(-200);
 
 
-	auto menu = Menu::create(itemsimple,itemordinary,itemdiffcult, nullptr);
+
+	auto menu = Menu::create(itemsimple,itemordinary,itemdiffcult,itemskin, nullptr);
 	this->addChild(menu);
 	//scheduleUpdate();
 	return true;
